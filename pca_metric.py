f02d456e10ff55c6123ae75679c29b3ebc4c6b66
Guillaume Leclerc <leclerc@mapmaker-gpu.csail.mit.edu>
2017-10-20 00:54:52 -0400

    PCA measurements and comparisons

--- pca_metric.py
+++ pca_metric.py
@@ -32,7 +32,7 @@ else:
     unwrap = wrap
 
 def init_models(count=REPLICATES):
-    return [wrap(MNIST_1h(s)) for s in [32, 128, 200, 568, 1024]]
+    return [wrap(MNIST_1h(s)) for s in [32, 64, 128, 200, 350, 568, 1024]]
 
 def save_model(model, id):
     with open('/tmp/model-%s.data' % id, 'wb+') as f:
@@ -63,7 +63,7 @@ def get_activations(model, loader):
     outputs = []
     for images, labels in loader:
         images = wrap(Variable(images, volatile=True))
-        outputs.append(model.partial_forward(images).data)
+        outputs.append(model.partial_forward(images, False).data)
     return torch.cat(outputs, 0)
 
 def simplify_all(values, basis):
@@ -71,7 +71,6 @@ def simplify_all(values, basis):
     result = D.clone().zero_()
     yield result.transpose(0, 1)
     for i in range(basis.size(0)):
-        print('hoho')
         basis_col = basis[:, i].unsqueeze(1)
         D_lines = D[i].unsqueeze(0)
         result += basis_col.mm(D_lines)
@@ -79,6 +78,15 @@ def simplify_all(values, basis):
 
 def score_simplification(values, simplified_values):
     return (values - simplified_values).pow(2).sum(1).mean()
+
+def get_distances(activations, pcas):
+    results = []
+    for a, pca in zip(activations, pcas):
+        errs = []
+        for reconstructed in simplify_all(a, wrap(torch.from_numpy(pca.components_))):
+            errs.append(score_simplification(a, reconstructed))
+        results.append(np.array(errs))
+    return results
 
 def get_dl(dataset, prefix):
     return DataLoader(
@@ -94,19 +102,47 @@ def get_dl(dataset, prefix):
 
 def get_pca(activations):
     pca = PCA()
-    pca.fit(unwrap(activations.numpy()))
+    pca.fit(unwrap(activations).numpy())
+    return pca
 
-def pipeline(dl):
+def plot_variance(variances, prefix):
+    plt.figure(figsize=(10, 5))
+    plt.title('%s - Explained variance per component for multiple models' % prefix)
+    for x in variances:
+        plt.plot(x, label=('%s neurons' % len(x)))
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.legend()
+    plt.xlabel('xth component (sorted)')
+    plt.ylabel('Explained variance')
+    plt.ylim((1e-13, 1e4))
+    plt.savefig('./plots/%s_1h_pca_explained_variance.png' % prefix)
+    plt.close()
+
+def plot_distances(distances, prefix):
+    plt.figure(figsize=(10, 5))
+    plt.title('%s - Square distance of PCA reconstruction' % prefix)
+    for x in distances:
+        plt.plot(x, label=('%s neurons' % len(x)))
+    plt.legend()
+    plt.xlabel('Number of components kept')
+    plt.ylabel('Mean distance (L2)')
+    plt.savefig('./plots/%s_1h_pca_reconstruction_distance.png' % prefix)
+    plt.close()
+
+
+def pipeline(ds, prefix):
+    dl = get_dl(ds, prefix)
     models = init_models()
     train(models, dl)
     activations = [get_activations(model, dl) for model in models]
+    pcas = [get_pca(x) for x in activations]
+    variances = [x.explained_variance_ for x in pcas]
+    distances = get_distances(activations, pcas)
+    plot_distances(distances, prefix)
+    plot_variance(variances, prefix)
+    return distances
 
-if False and __name__ == '__main__':
-    simple = get_dl(MNIST, 'MNIST')
-    complex = get_dl(FashionMNIST, 'FashionMNIST')
-
-    simple_models = init_models()
-    complex_models = init_models()
-    train(simple_models, simple)
-    train(complex_models, complex)
-
+if __name__ == '__main__':
+    pipeline(MNIST, 'MNIST')
+    pipeline(FashionMNIST, 'FashionMNIST')
