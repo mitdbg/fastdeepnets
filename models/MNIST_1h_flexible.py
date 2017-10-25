5c3390a55e76cbbd41933fda75f958338c75bcb2
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2017-10-24 22:46:41 -0400

    Improve stability + partial activations
    - logexp numerically stable for positive and negative arguments
    - Ability to compute partial activations in order to analyse the
    behavior of the model

--- models/MNIST_1h_flexible.py
+++ models/MNIST_1h_flexible.py
@@ -1,9 +1,14 @@
 import torch
 from torch.autograd import Variable
 from torch import nn
+from torch.nn.functional import sigmoid
 
 def stable_logexp(factor, k):
-    return (1 + (-factor * k).exp()).log() / k + factor
+    f = factor * k
+    if f.data.cpu().numpy()[0] < 0:
+        return (f.exp() + 1).log() / k
+    else:
+        return (1 + (-f).exp()).log() / k + factor
 
 def compute_loss(k, x_0, size):
         return stable_logexp(x_0, k) - stable_logexp(x_0 - size, k)
@@ -12,7 +17,7 @@ class MNIST_1h_flexible(nn.Module):
     def __init__(self, size, wrap):
         super(MNIST_1h_flexible, self).__init__()
         self.size = size
-        self.k = nn.Parameter(torch.ones(1))
+        self.k = Variable(torch.ones(1), requires_grad=False)
         self.x_0 = nn.Parameter(torch.ones(1) * size)
         self.hidden_layer = nn.Linear(28 * 28, size, bias=True)
         self.activation = nn.ReLU()
@@ -20,11 +25,16 @@ class MNIST_1h_flexible(nn.Module):
         self.output_layer = nn.Linear(size, 10, bias=True)
 
     def get_scaler(self):
-        return 1 - 1 / (1 + torch.exp(-self.k * (self.range - self.x_0)))
+        return sigmoid(-self.k * (self.range - self.x_0))
 
-    def forward(self, x):
+    def partial_forward(self, x):
         x = x.view(-1, 28*28)
         x = self.hidden_layer(x)
+        x = self.activation(x)
+        return x
+
+    def forward(self, x):
+        x = self.partial_forward(x)
         x = x * self.get_scaler()
         x = self.output_layer(x)
         return x
