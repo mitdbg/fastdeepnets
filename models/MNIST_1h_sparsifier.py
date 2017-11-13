7c87cfef4e86565c64ab0a44536bb6996c21bee9
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2017-11-13 15:37:00 -0500

    Add non-strict mode to the sparisfier network

--- models/MNIST_1h_sparsifier.py
+++ models/MNIST_1h_sparsifier.py
@@ -2,15 +2,16 @@ import torch
 from torch import nn
 
 class MNIST_1h_sparsifier(nn.Module):
-    def __init__(self, max_size, mode='strict'):
+    def __init__(self, max_size, strict=True):
         super(MNIST_1h_sparsifier, self).__init__()
-        self.mode = mode
+        self.strict = strict
         self.max_size = max_size
         self.hidden_layer = nn.Linear(28*28, max_size, bias=True)
         self.activation = nn.ReLU()
         self.filter = nn.Parameter(torch.ones(max_size))
         self.filter_activation = nn.ReLU()
         self.output_layer = nn.Linear(max_size, 10, bias=True)
+        self.stats_count = 0
 
     def get_filter(self):
         return self.filter_activation(self.filter)
@@ -27,8 +28,13 @@ class MNIST_1h_sparsifier(nn.Module):
         return x
 
     def loss(self):
-        return self.get_filter().sum()
+        if self.strict:
+            return self.get_filter().sum()
+        else:
+            return torch.abs(self.filter).sum()
+
+    def get_alive_neurons(self):
+        return self.get_filter() > 0
 
     def l0_loss(self):
-        return (self.get_filter() > 0).float().sum()
-
+        return self.get_alive_neurons().float().sum()
