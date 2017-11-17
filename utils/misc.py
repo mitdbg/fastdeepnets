9df18bb422f9b2648119818d2cd0f287643b0ace
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2017-11-17 18:32:59 -0500

    Update utils function
    - Allow to preload a dataset for faster training if there is enough RAM

--- utils/misc.py
+++ utils/misc.py
@@ -1,3 +1,20 @@
+from utils.wrapping import wrap
+import random
+
 def tn(x):
     return x.cpu().numpy()[0]
 
+class PreloadedDataloader():
+
+    def __init__(self, dl, shuffle=True, do_wrap=True):
+        self.data = list(dl)
+        if do_wrap:
+            self.data = [tuple(wrap(z) for z in x) for x in self.data]
+        self.shuffle = shuffle
+        self.dataset = dl.dataset
+
+    def __iter__(self):
+        copy = self.data[:]
+        if self.shuffle:
+            random.shuffle(copy)
+        return iter(copy)
