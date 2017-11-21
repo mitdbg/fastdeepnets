45ee55e31a4e22e7edb006e3e1956b40bb9b7780
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2017-11-20 23:44:37 -0500

    Add split to dataset

--- utils/misc.py
+++ utils/misc.py
@@ -6,15 +6,21 @@ def tn(x):
 
 class PreloadedDataloader():
 
-    def __init__(self, dl, shuffle=True, do_wrap=True):
-        self.data = list(dl)
-        if do_wrap:
-            self.data = [tuple(wrap(z) for z in x) for x in self.data]
+    def __init__(self, dl, shuffle=True, do_wrap=True, dataset=None):
+        self.data = dl
+        if not isinstance(self.data, list):
+            self.data = list(self.data)
+            if do_wrap:
+                self.data = [tuple(wrap(z) for z in x) for x in self.data]
         self.shuffle = shuffle
-        self.dataset = dl.dataset
+        self.dataset = dl.dataset if dataset is None else dataset
 
     def __iter__(self):
         copy = self.data[:]
         if self.shuffle:
             random.shuffle(copy)
         return iter(copy)
+
+    def split(self, portion):
+        split = int(portion * len(self.data))
+        return [PreloadedDataloader(x, self.shuffle, dataset=self.dataset) for x in [self.data[:split], self.data[split:]]]
