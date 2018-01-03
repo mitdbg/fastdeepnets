79adcb59d868ef889f21ac288b2d903aba49ec9e
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2018-01-02 20:49:35 -0500

    Update example.py with a "complex" CNN model

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from dynnet.layers import Linear, Input, BatchNorm1d, Conv2d
+from dynnet.layers import Linear, Input, BatchNorm1d, Conv2d, Flatten
 from dynnet.filters import SimpleFilter
 from dynnet.graph import Graph
 import torch
@@ -15,16 +15,8 @@ def forward(graph, mapping, target, optimizer):
     loss(output).backward()
     optimizer.step()
 
-# Fake input
+# Fully connected example
 x = Variable(torch.rand(3, 15))
-xx = Variable(torch.rand(3, 3, 16, 16))
-
-simple = Graph()
-i = simple.add(Input, 3, 16, 16)()
-o = simple.add(MaxPool2d, kernel_size=2)(i)
-
-
-
 graph = Graph()
 inp = graph.add(Input, 15)()
 fc1 = graph.add(Linear, out_features=20)(inp)
@@ -36,21 +28,29 @@ dr = graph.add(Dropout, 0.3)(bn)
 fc2 = graph.add(Linear, out_features=9)(dr)
 print(graph)
 optim = Adam(graph.parameters())
-
 forward(graph, {inp: x}, fc2, optim)
-
 filt.weight.data[2:5].zero_()
 filt2.weight.data[1:2].zero_()
 gc_log = graph.garbage_collect()
+# You need to think to update the optimizer
+# (at least optimizers that keep state in them)
 gc_log.update_optimizer(optim)
-
 forward(graph, {inp: x}, fc2, optim)
 print(graph)
 
+# CNN example
 graph2 = Graph()
-inp2 = graph2.add(Input, 3, 5, 5)()
+inp2 = graph2.add(Input, 3, 16, 16)()
 conv1 = graph2.add(Conv2d, out_channels=20, kernel_size=3)(inp2)
-x2 = Variable(torch.rand(2, 3, 5, 5))
+mp = graph2.add(MaxPool2d, kernel_size=2)(conv1)
+conv_fil = graph2.add(SimpleFilter)(mp)
+fl = graph2.add(Flatten)(conv_fil)
+fc3 = graph2.add(Linear, out_features=10)(fl)
+x2 = Variable(torch.rand(2, 3, 16, 16))
 print(graph2)
-result, = graph2({inp2: x2}, conv1)
+result, = graph2({inp2: x2}, fc3)
+conv_fil.weight.data[0:3].zero_()
+graph2.garbage_collect()
+print(graph2)
+
 
